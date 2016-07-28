893e1c6da1adf346fcb7e069a9cbea110a3f1042
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-07-27 20:24:10 -0700

    add inert module, init index.html, finish exercise 3

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+var Inert = require('inert');
 var server = new Hapi.Server();
 
 server.connection({
@@ -6,11 +7,16 @@ server.connection({
   port: Number(process.argv[2] || 8080)
 });
 
+server.register(Inert, function(err) {
+  if (err)
+    throw err;
+});
+
 server.route({
   path: '/',
   method: 'GET',
-  handler: function(request, reply) {
-    reply();
+  handler: {
+    file: 'index.html'
   }
 });
 
